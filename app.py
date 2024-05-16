d63592c4eea1d394e49e4c8031f899f3db3f35fa
Hennadii Borysevych <genuch3@gmail.com>
2024-05-16 11:44:20 +0200

    static url removed

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ config = dotenv_values(".env")
 openai.api_key = config['OPENAI_API_KEY']
 
 app = Flask(__name__,
-            template_folder="templates"
+            template_folder="templates",
+            static_folder="static",
+            static_url_path="/"
             )
 
 
