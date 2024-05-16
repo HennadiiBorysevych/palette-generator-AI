338750bb7ce9fa637fa87ea84994a98977fb8901
Hennadii Borysevych <genuch3@gmail.com>
2024-05-16 10:59:43 +0200

    app.js and app.css and app.py

--- app.py
+++ app.py
@@ -7,26 +7,48 @@ config = dotenv_values(".env")
 openai.api_key = config['OPENAI_API_KEY']
 
 app = Flask(__name__,
-    template_folder="templates"
-)
+            template_folder="templates"
+            )
+
+
+def get_colors(msg):
+    prompt = f"""
+    You are a color palette generating assistant that responds to text prompts for color palettes
+    Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
+    The palettes should be between 2 and 8 colors.
+
+    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
+    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
+    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
+
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: {msg}
+    A:
+    """
+    response = openai.chat.completions.create(
+        messages=[{"role": "user", "content": prompt}],
+        model="gpt-3.5-turbo",
+        max_tokens=200,
+    )
+
+    return json.loads(response.choices[0].message.content)
 
 
 @app.route('/')
 def index():
-    response = openai.chat.completions.create(
-        messages=[{"role": "user", "content": ''}],
-        model="gpt-3.5-turbo",
-        max_tokens=200,
-    )
-    day = response.choices[0].message.content
-    print(day)
-    return render_template('index.html', day=day)
+    return render_template('index.html')
 
 
 @app.route('/palette', methods=['POST'])
 def palette():
-    session['palette'] = request.form['palette']
-    return redirect(url_for('index'))
+    app.logger.info(request.form.get('query'))
+    colors = get_colors(request.form.get('query'))
+    return {"colors": colors}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
